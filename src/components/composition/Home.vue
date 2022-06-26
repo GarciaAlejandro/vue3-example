1782f9250670a20004419091d97433fa8433eac2
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-26 13:42:54 -0500

    example of reactive Vue3

---
+++ src/components/composition/Home.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+div {{counter.counter}}
+</template>
+<script>
+import { ref, reactive } from 'vue'
+export default {
+  name: 'UserRepositories',
+  setup () {
+    const text = ref('Hello World')
+    const obj = reactive({ counter: 0})
+
+    setInterval(() => obj.counter++, 1000)
+    return {
+      text,
+      counter: obj
+    }
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
