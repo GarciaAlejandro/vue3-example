74b67ef8bc15509a56cb8dadd9f1e9756f0957ab
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-25 19:31:37 -0500

    Transition Example

---
+++ src/pages/transitions/TransitionsExample.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+.q-pa-xl
+  q-btn(
+    color='primary',
+    @click="show = !show"
+  ) Menu
+  transition(name='fade' :duration="1000")
+    Menu(v-show="show")
+</template>
+<script>
+import Menu from '~/components/transitions/Menu.vue'
+export default {
+  name: 'TransitionsExample',
+  components: {
+    Menu
+  },
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang='sass'>
+@import '~/css/app.sass'
+</style>
