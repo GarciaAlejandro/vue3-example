4b427253814656cb00ca59998434e5b4285e5ab3
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-26 17:22:35 -0500

    Created a wrapper component to show the provider example

---
+++ src/components/provider/ProviderExample.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+div.q-pa-md
+  ChildComponent
+</template>
+<script>
+import ChildComponent from '~/components/provider/ChildComponent.vue'
+export default {
+  name: 'ProviderExample',
+  components: {
+    ChildComponent
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
