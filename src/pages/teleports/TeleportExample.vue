0d298de09103e437f28a465e4928992eb9f880e1
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-25 22:55:02 -0500

    Created the index page to the route

---
+++ src/pages/teleports/TeleportExample.vue
@@ -0,0 +1,24 @@
+<template lang='pug'>
+.q-pa-xl
+  Modal
+</template>
+<script>
+import Modal from '~/components/teleports/Modal.vue'
+export default {
+  name: 'TeleportExample',
+  components: {
+    Modal
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang='sass'>
+
+</style>
