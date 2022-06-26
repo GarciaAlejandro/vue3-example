dad4640606139b6646210a2c034da29e271f29fe
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-25 19:30:35 -0500

    Created component to apply the transition

---
+++ src/components/transitions/Menu.vue
@@ -0,0 +1,26 @@
+<template lang='pug'>
+ul
+  li Option 1
+  li Option 2
+  li Option 3
+  li Option 4
+</template>
+<script>
+export default {
+  name: 'Menu',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
