8058d553ca18dcff6c2738b0d9e7663a213e54ac
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-26 15:25:15 -0500

    Component to show the full name using Props & computed

---
+++ src/components/props/PropsExample.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+div {{firstName}} {{lastName}}
+div {{fullName}}
+
+</template>
+<script>
+import {  computed, toRefs } from 'vue'
+export default {
+  name: 'UserRepositories',
+  props: {
+    firstName: {
+      type: String,
+      default: 'John'
+    },
+    lastName: {
+      type: String,
+      default: 'Doe'
+    }
+  },
+  setup (props) {
+    const {firstName, lastName} = toRefs(props)
+    const fullName = computed(() => {
+      return `computed full name: ${firstName.value} ${lastName.value}`
+    })
+    return {
+      fullName
+    }
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
