f0fd03a822e10d0be3cea892dbd448ecd9fc9033
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-26 15:00:38 -0500

    Computed example using Vue3

---
+++ src/components/computed/ComputedExample.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+.text-h4.q-py-md Computed Example
+div {{firstName}} {{lastName}}
+div {{fullName}}
+
+</template>
+<script>
+import { ref, computed } from 'vue'
+export default {
+  name: 'UserRepositories',
+  setup () {
+    const firstName = ref('John')
+    const lastName = ref('Doe')
+    const fullName = computed(() => {
+      return `computed full name: ${firstName.value} ${lastName.value}`
+    })
+    return {
+      firstName,
+      lastName,
+      fullName
+    }
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
