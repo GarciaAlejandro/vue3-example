05764306c01065651c743d810bafba90383eaab7
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-25 19:31:09 -0500

    Added the route to lesson 6

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,11 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/AsyncComponent.vue') }]
   },
+  {
+    path: '/06-transitions',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/transitions/TransitionsExample.vue') }]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
