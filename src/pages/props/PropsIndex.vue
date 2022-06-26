311a929a9ddf3e59ceefe33cb58810e17ea9b220
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-26 15:24:46 -0500

    Added logic on the parent component to send props using Composition API

---
+++ src/pages/props/PropsIndex.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+div.q-pa-xl
+  .text-h4 Props Example
+  q-input(v-model="firstNameInput" @keyup="changeFirstName" label="Enter first name" outlined)
+  PropsExample(:first-name="firstName", last-name='Doe')
+</template>
+<script>
+import {ref} from 'vue'
+import PropsExample from '~/components/props/PropsExample.vue'
+export default {
+  name: 'PropsIndex',
+  components: {
+    PropsExample
+  },
+  setup () {
+    const firstName = ref('John')
+    const firstNameInput = ref('')
+    const changeFirstName = () => {
+      firstName.value = firstNameInput.value
+    }
+    return {
+      firstName,
+      firstNameInput,
+      changeFirstName
+    }
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
