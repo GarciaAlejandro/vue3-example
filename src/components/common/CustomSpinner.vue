63b8d56dcfd26481af2a14a1ceb9a3396a8a9401
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-25 22:54:30 -0500

    Created Custom spinners with its classes

---
+++ src/components/common/CustomSpinner.vue
@@ -0,0 +1,30 @@
+<template lang='pug'>
+.row
+  .col-12
+    div Loading ...
+  .col-12(v-if="loader === 'progress'")
+    div(class="progress")
+      div(class="color")
+  .col-12(v-if="loader === 'atom'")
+    div(class="loader q-ml-lg")
+</template>
+<script>
+export default {
+  name: 'CustomSpinner',
+  components: {
+
+  },
+  data () {
+    return {
+      loader: 'progress'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang='sass' scoped>
+@import '~/css/spinners-custom/progress.sass'
+@import '~/css/spinners-custom/atom.sass'
+</style>
