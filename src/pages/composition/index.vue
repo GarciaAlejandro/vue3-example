52b821865554d3f11e302ba4e22f1e2d74aa6f8c
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-26 13:42:41 -0500

    Created the index page for the new route

---
+++ src/pages/composition/index.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+div.flex.flex-center
+  Home
+</template>
+<script>
+import Home from '~/components/composition/Home.vue'
+export default {
+  name: 'Index',
+  components: {
+    Home
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
