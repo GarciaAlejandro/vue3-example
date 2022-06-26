c99343fd4c7ef723a63d256e104232e8c3d37b49
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-26 17:22:08 -0500

    Created the provider on Index page

---
+++ src/pages/provider/ProviderIndex.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+div.q-pa-xl
+  .text-h4 Provider / Inject example
+  q-input(v-model='username'  outlined label="Variable to send to Injector")
+  ProviderExample(firstName='John', lastName='Doe')
+</template>
+<script>
+import { provide, readonly, ref } from 'vue'
+import ProviderExample from '~/components/provider/ProviderExample.vue'
+
+export default {
+  name: 'ProviderIndex',
+  components: {
+    ProviderExample
+  },
+  setup () {
+    const username = ref('')
+    const text = ref('String readonly from Injector')
+    function updateUsername (value) {
+      username.value = value
+    }
+    provide('username', {username, updateUsername})
+    provide('text', readonly(text))
+
+    return {
+      username
+    }
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
